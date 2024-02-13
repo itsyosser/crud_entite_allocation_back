7202ec38996f43f0058e3226818ed2c3c0e6a477
itsyosser <yosser.tounsi@esprit.tn>
2024-02-13 13:56:16 +0100

    ft commit

---
+++ src/Controller/AllocationController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Allocation;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\AllocationType;
+use App\Repository\AllocationRepository;
+
+class AllocationController extends AbstractController
+{
+    #[Route('/allocation', name: 'app_allocation')]
+    public function index(): Response
+    {
+        return $this->render('allocation/index.html.twig', [
+            'controller_name' => 'AllocationController',
+        ]); 
+    }
+
+
+    #[Route('/admin', name: 'app_admin')]
+    public function indexadmin(AllocationRepository $repo): Response
+    {
+        $allocation=$repo->findAll();
+        return $this->render('admin/index.html.twig',[
+            'allocation' => $allocation,
+        ]); 
+    }
+  
+    #[Route('/showdballocation', name: 'showdballocation')]
+    public function showdballocation(AllocationRepository $allocationRepository ): Response
+    {  $allocation=$allocationRepository->findAll();
+        if (!$allocation) {
+            dump($allocation);
+        } 
+        return $this->render('admin/index.html.twig', [
+            'allocation' => $allocation
+        ]);}
+#[Route('/addformallocation', name: 'addformallocation')]
+    public function addformallocation(ManagerRegistry $managerRegistry, Request $req ): Response
+    {  $x=$managerRegistry->getManager();  
+        $allocation=new allocation();
+         $form =$this->createForm(AllocationType::class,$allocation);
+         $form->handleRequest($req); 
+         if($form->isSubmitted()and $form->isValid()){
+         $x->persist($allocation);
+          $x->flush();
+    
+          return $this->redirectToRoute('app_admin');} 
+        return $this->render('admin/addformallocation.html.twig', [ 
+        'form' => $form->createView(),
+        ]);
+}
+#[Route('/editallocation/{id}', name: 'editallocation')] 
+public function editallocation($id,AllocationRepository $allocationRepository,managerRegistry $managerRegistry,Request $req): Response  
+{ //var_dump(($id)).die();
+    $x=$managerRegistry->getManager();  
+     $dataid=$allocationRepository->find($id);
+    $form=$this->createForm(AllocationType::class,$dataid);
+    $form->handleRequest($req);
+    if($form->isSubmitted()and $form->isValid()){ 
+        $x->persist($dataid);
+        $x->flush();
+    
+        return $this->redirectToRoute('app_admin');}
+     return $this->renderForm('admin/editallocation.html.twig', [
+        'form' => $form
+    ]);
+} 
+#[Route('/deletallocation/{id}', name: 'deleteallocation')]
+public function deleteauthor($id,AllocationRepository $AllocationRepository,ManagerRegistry $managerRegistry): Response
+{
+    $em=$managerRegistry->getManager();
+    $dataid=$AllocationRepository->find($id);
+    $em->remove($dataid);
+    $em->flush();
+
+return $this->redirectToRoute('app_admin');  
+}
+}
---
+++ src/Entity/Allocation.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AllocationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AllocationRepository::class)]
+class Allocation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+ 
+  
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+   
+}
